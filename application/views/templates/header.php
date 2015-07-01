d3f21ab69dfc9895e008eae9a235944fc12fcd38
Emmanuel <sinnell@aol.com>
2015-07-01 23:38:02 +0000

    adjusted header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,7 +6,7 @@
 
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" charset="utf-8"> 
 
-	<link href="<?php echo base_url();?>css/style.css" rel="stylesheet" type='text/css'>
+	<link href="css/style.css" rel="stylesheet" type='text/css'>
 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
