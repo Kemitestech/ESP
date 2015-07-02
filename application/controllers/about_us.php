4e407adcc81d4ed049ea39ad318c14a4d1b0baa1
unuigbee <unuigbee@uni.coventry.ac.uk>
2015-07-02 21:47:41 +0100

    about_us controller index

---
+++ application/controllers/about_us.php
@@ -0,0 +1,26 @@
+<?php
+	class About_us extends CI_Controller {
+		
+		public function __construct(){
+			
+			parent::__construct();
+		}
+
+		public function index(){
+			
+			if ( ! file_exists(APPPATH.'views/about_pages/index.php')){ //checks if file exists
+				show_404();
+			}			
+
+			$data['title'] = 'Edward Street Parish'; //assigns title element of array to capitalized home page
+			$data['active']= 'active';			
+
+			$this->load->view('templates/header', $data);
+			$this->load->view('about_pages/index', $data);
+			$this->load->view('templates/newsletter_section', $data);
+			$this->load->view('templates/footer');			
+		}
+
+
+	}
+?>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -40,7 +40,8 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
 			    	<li class="<?php if(uri_string() == 'home'){echo $active;} ?>"><a href="home" class="nav-color">Home<span 
-class="sr-only">(current)</span></a></li>
+						class="sr-only">(current)</span></a>
+					</li>
 			    	<li class="dropdown">
           			<a href="#" class="dropdown-toggle nav-color" data-toggle="dropdown" role="button">About Us</a>
 		  				<ul class="dropdown-menu" role="menu">
