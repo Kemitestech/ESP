1317f32c99facd62e18594ba883f2d61a12b0683
unuigbee <unuigbee@uni.coventry.ac.uk>
2015-07-04 17:55:08 +0100

    Added prayer request

--- application/views/pages/prayer-request.php
+++ application/views/pages/prayer-request.php
@@ -1,1 +1,29 @@
-php
+<div class="jumbotron contact-background">    
+    <div class="container">
+        <form class="form-horizontal col-sm-10 col-sm-offset-3">
+          <div class="form-group">
+          <label class="col-sm-9">
+            <h1 class="header-title">Make a Prayer Request</h1>
+            <p>The power of prayer should never be under estimated. Let us help you in whatever circumstance. Use the form below for your prayer request and leave the rest to our Lord who never fails.
+               <a href="mailto:someone@example.com?Subject=Hello%20again" target="_top">prayer_request@cccedwardstreetparish.org</a>
+            </p>
+          </label>
+          </div>
+          <div class="form-group input-margin">
+           <div class="col-sm-9"> 
+            <input type="text" class="form-control input-lg no-radius" id="name1" placeholder="Name *">
+           </div> 
+          </div>                                
+          <div class="form-group input-margin">
+           <div class="col-sm-9"> 
+            <textarea id="InputRequest" class="form-control no-radius input-lg" name="Request" placeholder="Request *" rows="3"></textarea>
+           </div> 
+          </div> 
+          <div class="form-group">
+            <div class="col-sm-9"> 
+              <button type="submit" class="btn btn-info btn-lg btn-block no-radius">Send</button>
+            </div>
+          </div>  
+        </form>
+    </div>
+</div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -39,45 +39,47 @@
        </a>	
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
-		    	<li class="<?php if(uri_string() == 'home'){echo $active;} ?>"><a href="<?php echo base_url('home') ?>" 
-					class="nav-color">Home<span class="sr-only">(current)</span></a></li>
-		    	<li class="dropdown">
-      			<a href="<?php echo base_url('about-us') ?>" class="dropdown-toggle nav-color" data-toggle="dropdown" role="button">  				    About Us</a>
-		  		  <ul class="dropdown-menu" role="menu">
+			    	<li class="<?php if(uri_string() == 'home'){echo $active;} ?>"><a href="<?php echo base_url('home') ?>" 
+						class="nav-color">Home<span class="sr-only">(current)</span></a></li>
+			    	<li class="dropdown">
+	      			<a href="<?php echo base_url('about-us') ?>" class="dropdown-toggle nav-color" data-toggle="dropdown" role="button">  				    About Us</a>
+			  		  <ul class="dropdown-menu" role="menu">
 			            <li><a href="<?php echo base_url('about-us') ?>" class="nav-color">Who we are</a></li>
 				    	<li><a href="<?php echo base_url('about-us/history') ?>" class="nav-color">History</a></li>
 			            <li><a href="<?php echo base_url('about-us/days-of-worship') ?>" class="nav-color">Days of worship</a></li>
 			            <li><a href="<?php echo base_url('about-us/tenets') ?>" class="nav-color">Tenets</a></li>
-                  		<li><a href="#" class="nav-color">Constitution</a></li>
+	              		<li><a href="#" class="nav-color">Constitution</a></li>
 			            <li><a href="#" class="nav-color">Code of Conduct</a></li>
-		  		  </ul>
-        	    </li>
-            <li class="dropdown">
-			 <a href="#" class="dropdown-toggle nav-color" data-toggle="dropdown" role="button">News</a>
-             <ul class="dropdown-menu" role="menu"> 
-              <li><a href="newsletter.html" class="nav-color">Newsletters</a></li>
-              <li><a href="events.html" class="nav-color">Events</a></li>
-             </ul>  
-            </li>
-			<li class="<?php if(uri_string() == 'contact-us'){echo $active;} ?>"><a href="<?php echo base_url('contact-us') ?>" 
-			class="nav-color">Contact Us</a></li>
-            <li class="dropdown">
-			    	  <a href="#" class="dropdown-toggle nav-color" data-toggle="dropdown" role="button">Ministries</a>
-              <ul class="dropdown-menu" role="menu">
-                <li><a href="ministries.html" class="nav-color">Charity</a></li>
-                <li><a href="ministries.html" class="nav-color">Elder</a></li>
-                <li><a href="ministries.html" class="nav-color">Youth</a></li>
-                <li><a href="ministries.html" class="nav-color">Warden</a></li>
-                <li><a href="ministries.html" class="nav-color">Clergy</a></li>
-                <li><a href="ministries.html" class="nav-color">Warden</a></li>
-                <li><a href="ministries.html" class="nav-color">Bible Class</a></li>
-                <li><a href="ministries.html" class="nav-color">Choir</a></li>
-                <li><a href="ministries.html" class="nav-color">Building commitee</a></li>
-              </ul>
-            </li>
-            <li><a href="#" class="nav-color">Youth</a></li>
-			    	<li><a href="#" class="nav-color">Media</a></li>
-			    </ul>
-			</div>
+			  		  </ul>
+	        	    </li>
+	            <li class="dropdown">
+				 <a href="#" class="dropdown-toggle nav-color" data-toggle="dropdown" role="button">News</a>
+	             <ul class="dropdown-menu" role="menu"> 
+	              <li><a href="newsletter.html" class="nav-color">Newsletters</a></li>
+	              <li><a href="events.html" class="nav-color">Events</a></li>
+	             </ul>  
+	            </li>
+				<li class="<?php if(uri_string() == 'contact-us'){echo $active;} ?>"><a href="<?php echo base_url('contact-us')?>" 
+				class="nav-color">Contact Us</a></li>
+				<li class="<?php if(uri_string() == 'prayer-request'){echo $active;} ?>"><a href="<?php echo base_url('prayer-request')?>" 
+				class="nav-color">Prayer Request</a></li>
+	            <li class="dropdown">
+				 <a href="#" class="dropdown-toggle nav-color" data-toggle="dropdown" role="button">Ministries</a>
+	              <ul class="dropdown-menu" role="menu">
+	                <li><a href="ministries.html" class="nav-color">Charity</a></li>
+	                <li><a href="ministries.html" class="nav-color">Elder</a></li>
+	                <li><a href="ministries.html" class="nav-color">Youth</a></li>
+	                <li><a href="ministries.html" class="nav-color">Warden</a></li>
+	                <li><a href="ministries.html" class="nav-color">Clergy</a></li>
+	                <li><a href="ministries.html" class="nav-color">Warden</a></li>
+	                <li><a href="ministries.html" class="nav-color">Bible Class</a></li>
+	                <li><a href="ministries.html" class="nav-color">Choir</a></li>
+	                <li><a href="ministries.html" class="nav-color">Building commitee</a></li>
+	              </ul>
+	            </li>
+	            <li><a href="#" class="nav-color">Youth</a></li>
+				<li><a href="#" class="nav-color">Media</a></li>
+			</ul>
+		  </div>
 		</div>
 	</nav>
