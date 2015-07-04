0f42e57f457b67e32bf833b22476cc0c351d72e1
Emmanuel <sinnell@aol.com>
2015-07-04 19:54:43 +0000

    prayer-request view

--- application/views/pages/prayer-request.php
+++ application/views/pages/prayer-request.php
@@ -3,8 +3,9 @@
         <form class="form-horizontal col-sm-10 col-sm-offset-3">
           <div class="form-group">
           <label class="col-sm-9">
-            <h1 class="header-title">Make a Prayer Request</h1>
-            <p>The power of prayer should never be under estimated. Let us help you in whatever circumstance. Use the form below for your prayer request and leave the rest to our Lord who never fails.
+            <h1 class="header-title">Prayer Request</h1>
+            <p>Use the form below for your prayer request and 
+leave the rest to our Lord who never fails. Also you can email your request at
                <a href="mailto:someone@example.com?Subject=Hello%20again" target="_top">prayer_request@cccedwardstreetparish.org</a>
             </p>
           </label>
@@ -16,7 +17,7 @@
           </div>                                
           <div class="form-group input-margin">
            <div class="col-sm-9"> 
-            <textarea id="InputRequest" class="form-control no-radius input-lg" name="Request" placeholder="Request *" rows="3"></textarea>
+            <textarea id="InputRequest" class="form-control no-radius input-lg" name="Request" placeholder="Request *" rows="5"></textarea>
            </div> 
           </div> 
           <div class="form-group">
