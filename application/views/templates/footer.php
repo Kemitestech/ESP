4270eb23d76b2466147b2f5ce9cdccf6b421313d
unuigbee <unuigbee@uni.coventry.ac.uk>
2015-07-31 20:30:37 +0100

    Modified views

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,50 +1,69 @@
-	<footer class="footer"><!--start of footer-->
-  		<div class="container"><!--start of container-->
-  			<div class="page-header">
-  			  <div class="row">	
-  				<div class="col-xs-12 col-sm-6 col-md-8">
-  				  <h3 class="header-title">CCC Edward Street Parish</h3>
-  				</div>
-  				<div class="col-xs-6 col-md-4">
-  				  <ul class="nav">
-              <li class="horizontal-lists"><a href="#" class="header-title" style="font-size:14px;">FAQ</a></li> 
+	<div class="jumbotron newsletter-section">
+    <div class="container">
+      <form class="form-inline col-md-10 col-md-offset-1">
+        <h3 class="header-title center-text">Sign up to our newsletter</h3><p class="center-text">Coming soon</p><br><br>
+        <div class="form-group">
+          <input type="text" class="form-control" id="exampleInputName2" disabled="" placeholder="First Name">
+        </div>
+        <div class="form-group">
+          <input type="text" class="form-control" id="exampleInputName2" disabled="" placeholder="Last Name">
+        </div>        
+        <div class="form-group">
+          <input type="email" class="form-control" id="exampleInputEmail2" disabled="" placeholder="Email">
+        </div>
+        <button type="submit" disabled="" class="btn btn-default">Sign up</button>
+       </form>
+    </div>          
+  </div>    
+  <footer class="footer"><!--start of footer-->
+      <div class="container"><!--start of container-->
+        <div class="page-header">
+          <div class="row"> 
+          <div class="col-xs-10 col-sm-4 col-md-6">
+            <h3 class="header-title">CCC Edward Street Parish</h3>
+          </div>
+          <div class="col-xs-7 col-md-5 col-md-offset-1">
+        <ul class="nav">
+              <li class="horizontal-lists"><a href="faq.html" class="header-title" style="font-size:14px;">FAQ</a></li> 
               <li class="horizontal-lists"><a href="#" class="header-title" style="font-size:14px;">Terms of Use</a></li>
-              <li class="horizontal-lists"><a href="#" class="header-title" style="font-size:14px;">Privacy Policy</a></li>
+              <li class="horizontal-lists"><a href="#" class="header-title" style="font-size:14px;">Trademark</a></li>
+        <li class="horizontal-lists"><a href="#" class="header-title" style="font-size:14px;">Site Map</a></li>
             </ul> 
-  				</div>
-  			  </div>	
-			</div>
-		    <div class="row" style="margin-bottom: 20px;">	
-		    	<div class="col-xs-6 col-md-4">
-  					<h3 class="header-title">Service times</h3>
-  					<p>Sunday 10:00 - 14:00 AM</br></br> Wednesday 19:00 - 21:00 PM</br></br>Friday 19:00 - 21:00 PM</p>
-  				</div>
-  				<div class="col-xs-6 col-md-4">         
-  					<h3 class="header-title">Contact us</h3>
+          </div>
+          </div>  
+      </div>
+        <div class="row" style="margin-bottom: 20px;">  
+          <div class="col-xs-6 col-md-4">
+            <h3 class="header-title">Service times</h3>
+            <p>Sunday 10:00 - 14:00 AM</br></br> Wednesday 19:00 - 21:00 PM</br></br>Friday 19:00 - 21:00 PM</p>
+          </div>
+          <div class="col-xs-6 col-md-4">         
+            <h3 class="header-title">Contact us</h3>
             <address>
                 <strong>CCC Edward Street Parish</strong><br>
                 Saint Marks Centre<br>
                 London, SE14 6QX<br>
                 <abbr title="Phone">T:</abbr> 44 (0)208 692 9036
               </address>
-  				</div>
-  				<div class="col-xs-7 col-md-4">
-  					<h3 class="header-title">Social</h3>
+          </div>
+          <div class="col-xs-7 col-md-4">
+            <h3 class="header-title">Social</h3>
               <ul class="soc">
                   <li><a class="soc-facebook" href="#"></a></li>
                   <li><a class="soc-twitter" href="#"></a></li>
                   <li><a class="soc-youtube" href="#"></a></li>
                   <li><a class="soc-rss soc-icon-last" href="http://www.thenigerianvoice.com/news/179264/1/heritage-bank-pledges-consistent-and-seamless-serv.html" target="_blank"></a></li>
               </ul>
-		    </div>
-    	</div>
+        </div>
+      </div>
     </div><!--end of container-->
     <div class="container-fluid row-background">    
       <div class="container">  
         <div class="row"> 
           <div class="col-xs-6 col-md-4"><p>Copyright &#169 2015 CCC Edward Street Parish</p></div>
-          <div class="col-xs-6 col-md-4"><p>Web Design & Development by Sina Unuigbe</p></div>
-          <div class="col-xs-6 col-md-4">
+          <div class="col-xs-6 col-md-4"><p>Charity Registration Number: 5929215</p></div>
+      <div class="col-xs-6 col-md-4"><p>Designed and Developed by KemITes Tech</p></div>
+      <div class="col-xs-6 col-md-1 hidden-lg">
             <a href="#top" role="button" class="btn btn-default set-radius page-scroll">
               <span class="glyphicon glyphicon glyphicon-chevron-up"></span>
             </a>
@@ -52,25 +71,9 @@
         </div>
       </div>  
     </div>   
-	</footer><!--start of footer-->	
-  <script>
-      $('body').scrollspy({ 
-        target: '#navbar-example',
-        offset: $(window).height() * 0.2 
-
-      });
-
-      $('#myAffix').affix({
-          offset: {
-            top: $('.header_logo').height()-$('#nav').height()
-        }}); 
-
-      $('#sidebar').affix({
-        offset: {
-          top: $('.header_logo').height()+$('#myAffix').height()+$('.page-header').height()+$('.nav-tabs').height(),
-          bottom: 252
-        }
-        });  
-  </script>
+  <script src="<?php echo base_url('js/js/affixies.js') ?>"></script>
+  <script src="<?php echo base_url('js/sliders.js') ?>"></script>
+  <script src="<?php echo base_url('js/main.js') ?>"></script> 
+</footer><!--start of footer--> 
 </body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,78 +7,106 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" charset="utf-8"> 
 
 	<link href="<?php echo base_url('css/style.css') ?>" rel="stylesheet" type='text/css'>
-
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+	<link href="<?php echo base_url('css/bootstrap.min.css') ?>" rel="stylesheet" type='text/css'>
+	<link href="<?php echo base_url('css/bootstrap-theme.min.css') ?>" rel="stylesheet" type='text/css'>
+	<link href="<?php echo base_url('css/slick-theme.css') ?>" rel="stylesheet" type='text/css'>
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/jquery.slick/1.5.0/slick.css"/>
 	<link href='https://fonts.googleapis.com/css?family=Ubuntu:400,300' rel='stylesheet' type='text/css'>
 	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery.slick/1.5.0/slick.min.js"></script>
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js"></script>
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/plugins/ScrollToPlugin.min.js"></script>
 	<script src="<?php echo base_url('js/jquery.easing.min.js') ?>"></script>
   	<script src="<?php echo base_url('js/scrolling-nav.js') ?>"></script>
+  	<script src="<?php echo base_url('js/parallax.min.js') ?>"></script>
 </head>
 <body id="top">
-	<div class="header_logo">
-  		<div class="container-header">
-    	 	<img src="<?php echo base_url('img/edward-logo.png') ?>" alt="ccc-logo" class="ccc-logo img-thumbnail">
-  		</div>
-	</div>
-	<nav id="myAffix" class="navbar navbar-default">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-			        <span class="sr-only">Toggle navigation</span>
-			        <span class="icon-bar"></span>
-			        <span class="icon-bar"></span>
-			        <span class="icon-bar"></span>
-		    </button>
-			</div>
-		   <a class="navbar-brand" href="<?php echo base_url('home') ?>">
-        <h3 class="header-title" style="color: white !important;">CCC Edward Street Parish</h3>
-       </a>	
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
-			    	<li class="<?php if(uri_string() == 'home'){echo $active;} ?>"><a href="<?php echo base_url('home') ?>" 
-						class="nav-color">Home<span class="sr-only">(current)</span></a></li>
-			    	<li class="dropdown">
-	      			<a href="<?php echo base_url('about-us') ?>" class="dropdown-toggle nav-color" data-toggle="dropdown" role="button">  				    About Us</a>
-			  		  <ul class="dropdown-menu" role="menu">
-			            <li><a href="<?php echo base_url('about-us') ?>" class="nav-color">Who we are</a></li>
-				    	<li><a href="<?php echo base_url('about-us/history') ?>" class="nav-color">History</a></li>
-			            <li><a href="<?php echo base_url('about-us/days-of-worship') ?>" class="nav-color">Days of worship</a></li>
-			            <li><a href="<?php echo base_url('about-us/tenets') ?>" class="nav-color">Tenets</a></li>
-	              		<li><a href="#" class="nav-color">Constitution</a></li>
-			            <li><a href="#" class="nav-color">Code of Conduct</a></li>
-			  		  </ul>
-	        	    </li>
-	            <li class="dropdown">
-				 <a href="#" class="dropdown-toggle nav-color" data-toggle="dropdown" role="button">News</a>
-	             <ul class="dropdown-menu" role="menu"> 
-	              <li><a href="newsletter.html" class="nav-color">Newsletters</a></li>
-	              <li><a href="events.html" class="nav-color">Events</a></li>
-	             </ul>  
-	            </li>
-				<li class="<?php if(uri_string() == 'contact-us'){echo $active;} ?>"><a href="<?php echo base_url('contact-us')?>" 
-				class="nav-color">Contact Us</a></li>
-				<li class="<?php if(uri_string() == 'prayer-request'){echo $active;} ?>"><a href="<?php echo base_url('prayer-request')?>" 
-				class="nav-color">Prayer Request</a></li>
-	            <li class="dropdown">
-				 <a href="#" class="dropdown-toggle nav-color" data-toggle="dropdown" role="button">Ministries</a>
-	              <ul class="dropdown-menu" role="menu">
-	                <li><a href="ministries.html" class="nav-color">Charity</a></li>
-	                <li><a href="ministries.html" class="nav-color">Elder</a></li>
-	                <li><a href="ministries.html" class="nav-color">Youth</a></li>
-	                <li><a href="ministries.html" class="nav-color">Warden</a></li>
-	                <li><a href="ministries.html" class="nav-color">Clergy</a></li>
-	                <li><a href="ministries.html" class="nav-color">Warden</a></li>
-	                <li><a href="ministries.html" class="nav-color">Bible Class</a></li>
-	                <li><a href="ministries.html" class="nav-color">Choir</a></li>
-	                <li><a href="ministries.html" class="nav-color">Building commitee</a></li>
-	              </ul>
-	            </li>
-	            <li><a href="#" class="nav-color">Youth</a></li>
-				<li><a href="#" class="nav-color">Media</a></li>
+	<nav id="myAffix" class="myaffix1 navbar navbar-default">
+		<div class="row">
+			<div class="col-md-13">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="index.html">
+					<img src="<?php echo base_url('img/est-logo.png') ?>" alt="Edward Street Parish Logo" class="hidden-xs ccc-logo">
+					<h3 class="visible-xs-inline">CCC Edward Street Parish</h3>
+					</a>	
+				</div>
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav nav-font">
+			    	<li class="dropdown collapse-underline">
+          			<a class="dropdown-toggle nav-color nav-hover" data-toggle="dropdown" role="button">About</a>
+		  				<ul class="dropdown-menu" role="menu">
+							<li><a href="history.html" class="nav-color">Who we are</a></li>
+							<li><a href="history.html" class="nav-color">History</a></li>
+							<li><a href="mission-statement.html" class="nav-color">Tenets</a></li>
+							<li><a href="testimonials.html" class="nav-color">Tenets</a></li>
+							<li><a href="#" class="nav-color">Constitution</a></li>
+							<li><a href="code-of-conduct.html" class="nav-color">Code of conduct</a></li>
+							<li><a href="days-of-worship.html" class="nav-color">Days of worship</a></li>
+		  				</ul>
+					</li>
+            <li class="dropdown collapse-underline">
+				<a href="#" class="dropdown-toggle nav-color nav-hover" data-toggle="dropdown" role="button">News</a>
+             <ul class="dropdown-menu" role="menu">
+			  <li><a href="latest-news.html" class="nav-color">Latest news</a></li>
+              <li><a href="newsletter.html" class="nav-color">Newsletters</a></li>
+              <li><a href="events.html" class="nav-color">Events</a></li>
+             </ul>  
+            </li>
+			<li class="collapse-underline"><a href="contact.html" class="nav-color nav-hover">Contact</a></li>
+			<li class="collapse-underline"><a href="prayer-request.html" class="nav-color nav-hover">Prayer request</a></li>
+            <li class="dropdown collapse-underline">
+			  <a href="#" class="dropdown-toggle nav-color nav-hover" data-toggle="dropdown" role="button">Ministries</a>
+              <ul class="dropdown-menu" role="menu">
+				<li><a href="our-ministry.html" class="nav-color">Our Ministry</a></li>	
+                <li class="hidden-xs hidden-sm"><a href="ministries.html" class="nav-color">Parochial</a></li>
+                <li class="hidden-xs hidden-sm"><a href="ministries.html" class="nav-color">Elder</a></li>
+                <li class="hidden-xs hidden-sm"><a href="ministries.html" class="nav-color">Building commitee</a></li>
+                <li class="hidden-xs hidden-sm"><a href="ministries.html" class="nav-color">Youth</a></li>
+                <li class="hidden-xs hidden-sm"><a href="ministries.html" class="nav-color">Clergy</a></li>
+                <li class="hidden-xs hidden-sm"><a href="ministries.html" class="nav-color">Bible class</a></li>
+                <li class="hidden-xs hidden-sm"><a href="ministries.html" class="nav-color">Choir</a></li>
+                <li class="hidden-xs hidden-sm"><a href="ministries.html" class="nav-color">Warden</a></li>
+				<li class="hidden-xs hidden-sm"><a href="ministries.html" class="nav-color">Welfare</a></li>
+				<li class="hidden-xs hidden-sm"><a href="ministries.html" class="nav-color">Health and safety</a></li>
+              </ul>
+            </li>
+            <li class="collapse-underline"><a href="youth.html" class="nav-color nav-hover">Youth</a></li>
+			<li class="collapse-underline"><a href="gallery.html" class="nav-color nav-hover">Gallery</a></li>
 			</ul>
-		  </div>
+			<ul id="secondnav" class="nav navbar-nav repos-top-right">
+				<li class="hidden-xs hidden-sm"><a><img src="img/search2.png" class="nav-img-display" alt="" role="button" data-target="#collapseExample"data-toggle="collapse" aria-expanded="false" aria-controls="collapseExample"></a></li>
+				  <form class="navbar-form navbar-left navbar-form-override visible-xs-inline visible-sm-inline" role="search">
+					<div class="form-group">
+					 <div class="col-xs-7 col-sm-8 search-pos"> 
+					  <input type="text" class="form-control" placeholder="Search">
+					 </div>
+					 <button type="submit"style="margin-left: 20px;" class="btn btn-default">Search</button>		
+					</div>
+					
+				  </form>
+				<li class="visible-lg-inline"><a href="#" class="nav-color"><img src="img/user.png" style="" class="nav-img-display" alt=""></a></li>
+			</ul>
+			<div class="collapse" id="collapseExample">
+				<form class="navbar-form navbar-left" role="search">
+					<div class="form-group">
+					<div class="input-group">
+					  <input type="text" class="form-control border-style" placeholder="Search">
+					  <span class="input-group-btn nav-inputsearch">
+						<img src="img/search2.png" class="nav-img-display" role="button">
+					  </span>
+					</div>
+					</div>
+				</form>
+			</div>			
 		</div>
+		</div>
+	</div>
 	</nav>
