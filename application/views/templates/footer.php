c35bfbdc16dc75e19419f102219b78835b65a9ad
unuigbee <unuigbee@uni.coventry.ac.uk>
2015-07-31 20:42:47 +0100

    Footer corrections

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,21 +1,4 @@
-	<div class="jumbotron newsletter-section">
-    <div class="container">
-      <form class="form-inline col-md-10 col-md-offset-1">
-        <h3 class="header-title center-text">Sign up to our newsletter</h3><p class="center-text">Coming soon</p><br><br>
-        <div class="form-group">
-          <input type="text" class="form-control" id="exampleInputName2" disabled="" placeholder="First Name">
-        </div>
-        <div class="form-group">
-          <input type="text" class="form-control" id="exampleInputName2" disabled="" placeholder="Last Name">
-        </div>        
-        <div class="form-group">
-          <input type="email" class="form-control" id="exampleInputEmail2" disabled="" placeholder="Email">
-        </div>
-        <button type="submit" disabled="" class="btn btn-default">Sign up</button>
-       </form>
-    </div>          
-  </div>    
-  <footer class="footer"><!--start of footer-->
+<footer class="footer"><!--start of footer-->
       <div class="container"><!--start of container-->
         <div class="page-header">
           <div class="row"> 
