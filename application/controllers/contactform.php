dac89cb24d6883f46811261b696fd756a42ce7cb
unuigbee <unuigbee@uni.coventry.ac.uk>
2015-08-07 22:42:01 +0100

    Test form

--- application/controllers/contactform.php
+++ application/controllers/contactform.php
@@ -6,15 +6,41 @@ class Contactform extends CI_Controller {
 		parent::__construct();
 		
 	}
+    
+    public function alpha_space_only($str)
+    {
+        if (!preg_match("/^[a-zA-Z ]+$/",$str))
+        {
+            $this->form_validation->set_message('alpha_space_only', 'The %s field must contain only alphabets and space');
+            return FALSE;
+        }
+        else
+        {
+            return TRUE;
+        }
+    }
 
     public function index(){
         $this->load->library('form_validation');
            	
+        $this->form_validation->set_rules('fullname', 'Full Name', 'trim|required|xss_clean|callback_alpha_space_only');
+        $this->form_validation->set_rules('businessname', 'Business Name', 'trim|xss_clean|callback_alpha_space_only');
+        $this->form_validation->set_rules('email', 'Email Address', 'trim|required|valid_email');
+        $this->form_validation->set_rules('phone', 'Telephone Number', 'trim|is_natural|min_length[11]');
+        $this->form_validation->set_rules('subject', 'Subject', 'trim|required|xss_clean|callback_alpha_space_only');
+        $this->form_validation->set_rules('message', 'Enquiry', 'trim|required|xss_clean');
+
         if($this->form_validation->run() == FALSE){
+            $this->load->view('templates/header');
             $this->load->view('pages/contact-us');
+            $this->load->view('templates/newsletter_section');
+            $this->load->view('templates/footer');  
         }
         else{
+            $this->load->view('templates/header');
             $this->load->view('pages/home');
+            $this->load->view('templates/newsletter_section');
+            $this->load->view('templates/footer');
         }
     }
 
