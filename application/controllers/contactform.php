866f80a535c5edf169efd121ce023b72650aa246
unuigbee <unuigbee@uni.coventry.ac.uk>
2015-08-08 14:29:45 +0100

    Test form

--- application/controllers/contactform.php
+++ application/controllers/contactform.php
@@ -60,6 +60,7 @@ class Contactform extends CI_Controller {
             $this->data['message'] = 'Success';
             echo json_encode($this->data);
 
+
             //$msg = array(
             //        'fullname' => form_error('fullname'), 
             //        'business' => form_error('businessname'),
@@ -68,17 +69,22 @@ class Contactform extends CI_Controller {
             //        'subject' => form_error('subject'),
             //        'message' => form_error('message')
             //);    
-            
-            
-                //$this->email->from('sinnell@aol.com', 'Emmanuel');
-                //$this->email->to('info.cccedwardstreetparish.org');
-                //$this->email->subject('Email Test');
-                //$this->email->message('Testing the email class.');
+            $config['protocol'] = 'smtp';
+            $config['charset']  = 'iso-8859-1'; //Change this you your preferred charset 
+            $config['wordwrap'] = TRUE;
+            $config['mailtype'] = 'html'; //Use 'text' if you don't need html tags and images
 
-                //$this->email->send(FALSE);
-                //$this->email->print_debugger(array('headers', 'body', 'subject'));
+            $config['smtp_host'] = '#';
+            $config['smtp_user'] = '#';
+            $config['smtp_pass'] = '#';
+            $config['smtp_port'] = '#';   
             
-           
+            $this->email->from('sinnell@aol.com', 'Emmanuel');
+            $this->email->to('info.cccedwardstreetparish.org');
+            $this->email->subject('Email Test');
+            $this->email->message('If you forgot how to do this, go ahead and refer to: <a href="http://the-amazing-php.blogspot.com/2015/05/codeigniter-send-email-with-mandrill.html">The Amazing PHP</a>.');
+
+            $this->email->send();
         }
         
         
