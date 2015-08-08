1c02316072c2ac582d768912374b4c5fb65f4869
unuigbee <unuigbee@uni.coventry.ac.uk>
2015-08-08 04:03:22 +0100

    Test form

--- application/controllers/contactform.php
+++ application/controllers/contactform.php
@@ -7,42 +7,32 @@ class Contactform extends CI_Controller {
 		
 	}
     
-    public function alpha_space_only($str)
-    {
-        if (!preg_match("/^[a-zA-Z ]+$/",$str))
-        {
-            $this->form_validation->set_message('alpha_space_only', 'The %s field must contain only alphabets and space');
-            return FALSE;
-        }
-        else
-        {
-            return TRUE;
-        }
-    }
 
     public function index(){
         $this->load->library('form_validation');
         $this->load->helper('form');
            	
-        $this->form_validation->set_rules('fullname', 'Full Name', 'trim|required|xss_clean|callback_alpha_space_only');
-        $this->form_validation->set_rules('businessname', 'Business Name', 'trim|required|xss_clean|callback_alpha_space_only');
+        $fullname = $this->input->post('fullname');
+        $businessname = $this->input->post('businessname');
+        $email = $this->input->post('email');
+        $phone = $this->input->post('phone');
+        $subject = $this->input->post('subject');
+        $message = $this->input->post('message');
+        $ajax = $this->input->post('ajax');
+        
+        $this->form_validation->set_rules('fullname', 'Full Name', 'min_length[3]|trim|required|xss_clean|alpha_numeric_spaces');
+        $this->form_validation->set_rules('businessname', 'Business Name', 'trim|required|xss_clean|alpha_numeric_spaces');
         $this->form_validation->set_rules('email', 'Email Address', 'trim|required|valid_email');
         $this->form_validation->set_rules('phone', 'Telephone Number', 'trim|is_natural|min_length[11]');
-        $this->form_validation->set_rules('subject', 'Subject', 'trim|required|xss_clean|callback_alpha_space_only');
-        $this->form_validation->set_rules('message', 'Enquiry', 'trim|required|xss_clean');
+        $this->form_validation->set_rules('subject', 'Subject', 'trim|required|xss_clean|alpha_numeric_spaces');
+        $this->form_validation->set_rules('message', 'Enquiry', 'trim|required|xss_clean|alpha_numeric_spaces');
+        $this->form_validation->set_rules('ajax', 'Ajax', 'required|is_natural|alpha_numeric_spaces');
 
         $data['active'] = 'active';
         $data['title'] = 'Edward Street Parish';
+            
 
         if($this->form_validation->run() == FALSE){
-            
-            $fullname = $this->input->post('fullname');
-            $businessname = $this->input->post('businessname');
-            $email = $this->input->post('email');
-            $phone = $this->input->post('phone');
-            $subject = $this->input->post('subject');
-            $message = $this->input->post('message');
-            $ajax = $this->input->post('ajax');
             
             if($ajax){
                 $this->load->view('templates/header', $data);
