cce9e30ee5b749c60bb3dba26b96c23ea8fcd757
unuigbee <unuigbee@uni.coventry.ac.uk>
2015-08-08 15:04:49 +0100

    Test form

--- application/controllers/contactform.php
+++ application/controllers/contactform.php
@@ -70,14 +70,23 @@ class Contactform extends CI_Controller {
             //        'message' => form_error('message')
             //);    
               
-            
+            $config['protocol'] = 'smtp';
+            $config['charset']  = 'iso-8859-1'; //Change this you your preferred charset 
+            $config['wordwrap'] = TRUE;
+            $config['mailtype'] = 'html'; //Use 'text' if you don't need html tags and images
+
+            $config['smtp_host'] = 'smtp.mandrillapp.com';
+            $config['smtp_user'] = 'info@cccedwardstreetparish.org';
+            $config['smtp_pass'] = 'spZUYyrkNIpqIBo4LZT7BA';
+            $config['smtp_port'] = '587';
+                        
             $this->email->from('sinnell@aol.com', 'Emmanuel');
             $this->email->to('info.cccedwardstreetparish.org');
             $this->email->subject('Email Test');
             $this->email->message('If you forgot how to do this, go ahead and refer to: <a href="http://the-amazing-php.blogspot.com/2015/05/codeigniter-send-email-with-mandrill.html">The Amazing PHP</a>.');
 
-            $this->email->send(FALSE);
-            echo $this->email->print_debugger(array('headers', 'subject', 'body'));
+            $this->email->send();
+            //echo $this->email->print_debugger(array('headers', 'subject', 'body'));
         }
         
         
