e25f86ba7d59ebdb129883bc367169e732e1f9ff
Emmanuel <unuigbee@uni.coventry.ac.uk>
2015-08-19 00:16:21 +0100

    Restructured ministry page

--- application/views/ministry_pages/our-ministries.php
+++ application/views/ministry_pages/our-ministries.php
@@ -4,104 +4,61 @@
 		<p>Duis vulputate mauris nec facilisis gravida. Quisque vel elementum lacus. In luctus posuere augue id condimentum. Aliquam sed convallis libero, varius faucibus tellus. Curabitur ultrices eleifend molestie.<br><br></p>
 		<p>View our different Ministries</p><br><br>
 		<div class="row">
-                        
-			<div class="col-md-3 col-xs-6 col-sm-6">
+            <div class="col-md-3 col-xs-6 col-sm-6">
 				<a href="#">
-				<div style="background-color: #FFB100; padding: 6px; color: #333;">
-					<img src="<?php echo base_url('img/iconfinder/parochial.svg') ?>" class="img-responsive " alt="image">
-					<h3>Parochial Committee</h3>
-				</div>
-				</a>	
-			</div>
-			
-			<div class="col-md-3 col-xs-6 col-sm-6">
-			<a href="#">
-			<div style="background-color: #00A0C6; padding: 6px; color: #333;"> 
-			  <img src="<?php echo base_url('img/iconfinder/brick.svg') ?>" class="img-responsive" alt="image">  
-			  <h3>Building Committee</h3>
-			 </div> 
-			</a>	
-			</div>
-			
-			<div class="col-md-3 col-xs-6 col-sm-6">
-			<a href="#">
-			<div style="background-color: #4ABC96; padding: 6px; color: #333;">
-				<img src="<?php echo base_url('img/iconfinder/bible_class.svg') ?>" class="img-responsive img-rounded" alt="image">  
-				<h3>Bible Class</h3>
-			</div>
-			</a>	
-			</div> 
-			
-			<div class="col-md-3 col-xs-6 col-sm-6">
-				<a href="#">
-				<div style="background-color: #EAA300; padding: 6px; color: #333;">
-					<img src="<?php echo base_url('img/iconfinder/choir.svg') ?>" class="img-responsive img-rounded" alt="image">				
+				<div class="bg-orange flaticon_thumbnail">
+					<img src="<?php echo base_url('img/iconfinder/choir.svg') ?>" class="img-responsive" alt="image">				
 					<h3>Choir</h3>		
 				</div> 
 				</a> 	
 			</div>
-
-      
+				
+			<div class="col-md-3 col-xs-12 col-sm-6">
+				<a href="#">
+					<div class="bg-green flaticon_thumbnail">
+						<img src="<?php echo base_url('img/iconfinder/sunday_school.svg') ?>" class="img-responsive" alt="image">
+						<h3>Sunday School</h3>
+					</div>  
+				</a> 	
+			</div>
+			
+			<div class="col-md-3 col-xs-12 col-sm-6">
+				<a href="#">
+					<div class="bg-blue flaticon_thumbnail">
+						<img src="<?php echo base_url('img/iconfinder/Group_of_hands.svg') ?>" class="img-responsive" alt="image">
+						<h3>Youth</h3>  
+					</div>
+				</a>
+			</div>
+			
+			<div class="col-md-3 col-xs-12 col-sm-6">
+				<a href="#">
+					<div class="bg-torqouise flaticon_thumbnail"> 
+					  <img src="<?php echo base_url('img/iconfinder/dove.svg') ?>" class="img-responsive" alt="image">  
+					  <h3>Prophet</h3>
+					</div> 
+				</a>	
+			</div>
 		</div>
 		<hr>
     <div class="row">
-        <div class="col-md-3 col-xs-6 col-sm-6">
-			<a href="#">
-				<div style="background-color: #FFB100; padding: 6px; color: #333;">
-				   <img src="<?php echo base_url('img/iconfinder/elder1.svg') ?>" class="img-responsive" alt="image">  
-				   <h3>Elder</h3>
-				</div>
-			</a> 	
-        </div>
-        
-		<div class="col-md-3 col-xs-6 col-sm-6">
-			<a href="#">
-				<div style="background-color: #00A0C6; padding: 6px; color: #333;">
-					<img src="<?php echo base_url('img/iconfinder/health-and-safety.svg') ?>" class="img-responsive img-rounded" alt="image">
-					<h3>Health and Safety</h3>   
-				</div>
-			</a>	
-        </div>	
-		
-		<div class="col-md-3 col-xs-6 col-sm-6">
-			<a href="#">
-			<div style="background-color: #4ABC96; padding: 6px; color: #333;">
-				<img src="<?php echo base_url('img/iconfinder/Group_of_hands.svg') ?>" class="img-responsive img-rounded" alt="image">
-				<h3>Youth</h3>  
-			</div>
-			</a>
-        </div>
-		
-        <div class="col-md-3 col-xs-6 col-sm-6">
-			<a href="#">
-				<div style="background-color: #EAA300; padding: 6px; color: #333;">
-					<img src="<?php echo base_url('img/iconfinder/clergy.svg') ?>" class="img-responsive img-rounded" alt="image">
-					<h3>Clergy</h3>
-				</div>	
-			</a>
-        </div>
-        
-          
+		<div class="col-md-3 col-xs-12 col-sm-6">
+				<a href="#">
+					<div class="bg-orange flaticon_thumbnail">
+						<img src="<?php echo base_url('img/iconfinder/warden.svg') ?>" class="img-responsive" alt="image">
+						<h3>Warden</h3>
+					</div>  
+				</a> 	
+			</div>	
+			
+			<div class="col-md-3 col-xs-12 col-sm-6">
+				<a href="#">
+					<div class="bg-green flaticon_thumbnail">
+						<img src="<?php echo base_url('img/iconfinder/bible_class.svg') ?>" class="img-responsive img-rounded" alt="image">  
+						<h3>Bible Class</h3>
+					</div>
+				</a>	
+			</div>      
     </div>
-	<hr>
-	<div class="row">
-		<div class="col-md-3 col-xs-6 col-sm-6">
-			<a href="#">
-				<div style="background-color: #FFB100; padding: 6px; color: #333;">
-					<img src="<?php echo base_url('img/iconfinder/warden.svg') ?>" class="img-responsive img-rounded" alt="image">
-					<h3>Warden</h3>
-				</div>  
-			</a> 	
-        </div>
-		<div class="col-md-3 col-xs-6 col-sm-6">
-			<a href="#">
-				<div style="background-color: #00A0C6; padding: 6px; color: #333;">
-					<img src="<?php echo base_url('img/iconfinder/charity.svg') ?>" class="img-responsive img-rounded" alt="image">
-					<h3>Welfare</h3>    
-				</div>
-			</a> 	
-        </div>	    	
-	</div>
-    
     </div>  
   </div>
