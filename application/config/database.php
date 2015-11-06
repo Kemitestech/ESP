d0d63089d22906457a5ec26eb2658292f1878a5a
Emmanuel <sinnell@aol.com>
2015-11-06 00:02:58 +0000

    correction database 2

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'editor';
+$db['default']['password'] = 'Mw2akimbo.';
+$db['default']['database'] = 'edwardstreet';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
