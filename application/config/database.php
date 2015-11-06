4c00aa21b1bf3a0fe843893b0e464d746202eedc
Emmanuel <unuigbee@users.noreply.github.com>
2015-11-06 00:08:05 +0000

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'editor';
-$db['default']['password'] = 'Mw2akimbo.';
-$db['default']['database'] = 'edwardstreet';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
