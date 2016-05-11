17d5f88caa32fba2c3432e4b57606f5cac7b5ecb
Emmanuel <sinnell@aol.com>
2016-05-11 13:05:31 +0000

    moved assets to assets folder

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,30 @@
-$(function(){
-
+$('body').scrollspy({ 
+	target: '#navbar-example',
+	offset: $(window).height() * 0.2 
 });
+
+$('.parallax-window').parallax({imageSrc: 'img/light_cross.jpg', bleed: 200});
+
+$(function(){	
+
+	var $window = $(window);
+	var scrollTime = 0.3;
+	var scrollDistance = 120;
+
+	$window.on("mousewheel DOMMouseScroll", function(event){
+
+		event.preventDefault();	
+
+		var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+		var scrollTop = $window.scrollTop();
+		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+
+		TweenMax.to($window, scrollTime, {
+			scrollTo : { y: finalScroll, autoKill:true },
+				ease: Power1.easeOut,
+				overwrite: 5							
+			});
+
+	});
+});
+
