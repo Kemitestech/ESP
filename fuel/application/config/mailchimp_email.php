cfac4861cb7364220454f2e733d05ef1fe24d432
Emmanuel <sinnell@aol.com>
2016-05-13 20:07:19 +0000

    updated gitignore to ignore mailchimp config

---
+++ fuel/application/config/mailchimp_email.php
@@ -0,0 +1,14 @@
+$config['protocol'] = 'smtp'; 
+$config['charset'] = 'utf-8';
+$config['wordwrap'] = TRUE;  
+$config['mailtype'] = "html";
+$config['crlf'] = '\r\n'; 
+$config['newline'] = '\r\n';
+
+$config['to_address'] = '';
+
+// Change this information to match your mailchimp api settings
+$config['smtp_host'] = '';
+$config['smtp_user'] = '';
+$config['smtp_pass'] = '';
+$config['smtp_port'] = '587';
