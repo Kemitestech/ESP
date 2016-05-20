e8f3e65e438bcd5742c61c5c8892bfbd0967b8d9
Alexis Mclaren <mclaren@minibian>
2016-05-20 12:28:59 +0100

    added titles for the about us pages

--- fuel/application/views/_variables/about-us.php
+++ fuel/application/views/_variables/about-us.php
@@ -1,4 +1,10 @@
 <?php 
 $pages = array();
-
+// Titles for about-us pages 
 $pages['about-us/our-ministries'] = array('page_title' => ' Our Ministries - CCC Edward Street Parish'); 
+$pages['about-us/what-we-do'] = array('page_title' => 'What we do - CCC Edward Street Parish');
+$pages['about-us/our-departments'] = array('page_title' => 'Our Departments - CCC Edward Street Parish');
+$pages['about-us/tenets'] = array('page_title' => 'Tenets - CCC Edward Street Parish');
+$pages['about-us/testimonials'] = array('page_title' => 'Testimonials - CCC Edward Street Parish');
+$pages['about-us/worship-with-us'] = array('page_title' => 'Worship With Us - CCC Edward Street Parish');
+$pages['about-us/history'] = array('page_title' => 'History - CCC Edward Street Parish');
