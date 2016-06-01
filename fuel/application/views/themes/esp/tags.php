f6a9d8cb4c554a670e5bb6ff76c6d80e1e407549
Emmanuel <sinnell@aol.com>
2016-06-01 21:59:10 +0000

    added same view style of categories to tags page

--- fuel/application/views/themes/esp/tags.php
+++ fuel/application/views/themes/esp/tags.php
@@ -1,13 +1,15 @@
-<h1>Tags</h1>
 <?php foreach($tags as $tag) :
 		$posts = $tag->posts;
 		if (!empty($posts)) :
 	 ?>
-<h2><?=fuel_edit($tag->id, 'Edit Category', 'blog/tags')?><?=anchor($tag->url, $tag->name)?></h2>
-	<ul class="bullets">
-	<?php foreach($posts as $post) : ?>
-		<li><?=fuel_edit($post->id, 'Edit Post', 'blog/posts')?><?=anchor($this->fuel_blog->url('id/'.$post->id), $post->title)?></li>
-	<?php endforeach; ?>
-	</ul>
+	<div class="row">
+		<div class="col-md-12 col-sm-12 col-md-offset-2 col-sm-offset-2">	
+			<a href="<?=$tag->url?>" class="filter">
+			<?=fuel_edit($tag->id, 'Edit Category', 'blog/tags')?>
+				<h1 class="filter-item filter-name"><?=$tag->name?></h1>
+				<div class="filter-item filter-count"><span class="badge"><?=count($posts)?></span></div>
+			</a>
+		</div>	
+	</div>
 <?php endif; ?>
 <?php endforeach; ?>
