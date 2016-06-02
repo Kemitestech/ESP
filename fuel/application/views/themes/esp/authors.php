39ee40ee0be99e7067379063b43bc9a1376c64bf
Emmanuel <sinnell@aol.com>
2016-06-02 12:39:39 +0000

    implemented author list view

--- fuel/application/views/themes/esp/authors.php
+++ fuel/application/views/themes/esp/authors.php
@@ -1,10 +1,13 @@
-<h1>Authors</h1>
-
 <?php foreach($authors as $author) : ?>
-<div class="author">
-	<?php if (!empty($author->avatar_image)) : ?>
-	<?=$author->get_avatar_img_tag(array('class' => 'img_left', 'alt' => $author->name))?>
-	<?php endif; ?>
-	<?=anchor($author->url, $author->name)?>
+<div class="row">
+		<div class="col-md-12 col-sm-12 col-md-offset-2 col-sm-offset-2">	
+			<a href="<?=$author->url?>" class="filter">			
+					<?php if (!empty($author->avatar_image)) : ?>
+					<?=$author->get_avatar_img_tag(array('class' => 'img_left', 'alt' => $author->name))?>
+					<?php endif; ?>
+					<h1 class="filter-item filter-name"><?=$author->name?></h1>
+				<div class="filter-item filter-count"><span class="badge"><?=count($author->posts)?></span></div>
+			</a>
+		</div>	
 </div>
 <?php endforeach; ?>
