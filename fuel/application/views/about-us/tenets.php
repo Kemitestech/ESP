56727e57ec699941ea06ee52e7d1d5a998b9c893
Emmanuel <sinnell@aol.com>
2016-06-30 19:55:20 +0000

    cleaned up tenets page

--- fuel/application/views/about-us/tenets.php
+++ fuel/application/views/about-us/tenets.php
@@ -1,160 +1,125 @@
-<div class="jumbotron contact-background reset-jumb-pos">    
+<div class="jumbotron contact-background reset-jumb-pos">
     <div class="container">
-     
-       <h1 class="header-title ">Our Tenets</h1><br><br>
-       
-	   <blockquote>
-		<p>Let my teaching fall like rain and my words descend like dew, like showers on new grass, like abundant rain on tender plants.</p>
-		<footer><cite title="Source Title">Deuteronomy 32:2</cite></footer>
-	   </blockquote>
-		<p> Each of the tenets below is supported with one or more biblical reference as every aspect of our worship will be found in the scriptures <br></br></p>
-		<div class="row">
-                        
-        <div class="col-xs-6 col-sm-3 col-md-3">
-            <a data-toggle="modal" data-target="#myModal">
-              <p class="bg-orange" style="padding: 10px">Members must not take any alcoholic drink or intoxicant</p>
-            </a>  
-            <div class="caption">  
-              <p>Matthew 17: 1 - 2 <br> Matthew 28: 2 - 3<br>Mark 9: 2 - 3<br>
-				Revelation 7: 9
-			  </p>
-            </div>  
+      <h1 class="header-title ">Our Tenets</h1>
+
+      <blockquote>
+		      <p>Let my teaching fall like rain and my words descend like dew, like showers on new grass, like abundant rain on tender plants.</p>
+		      <footer><cite title="Source Title">Deuteronomy 32:2</cite></footer>
+      </blockquote>
+		  <p> Each of the tenets below is supported with one or more biblical references as every aspect of our worship will be found in the scriptures <br></br></p>
+		  <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-3">
+          <a>
+            <p class="bg-orange tenet">It is essential to worship GOD</p>
+          </a>
+          <div class="caption">
+              <p>Exodus 34: 8<br> Psalms 99: 5<br>Revelations 14: 7<br> Matthew 4: 10</p>
+          </div>
         </div>
-        
-        <div class="col-xs-6 col-sm-3 col-md-3">
-            <a data-toggle="modal" data-target="#myModal">
-              <p class="bg-blue" style="padding: 10px">We use white cloth for the sutana we wear for worship</p>
-            </a>  
-            <div class="caption">  
-              <p>Matthew 17: 1 - 2 <br> Matthew 28: 2 - 3<br>Mark 9: 2 - 3<br>
-				Revelation 7: 9
-			  </p>
-            </div>  
+
+        <div class="col-xs-12 col-sm-6 col-md-3">
+          <a>
+            <p class="bg-torqouise tenet">
+              Women on their menstrual period should abstain from the church for 7 clear days until the 8th day to come for santisfication
+            </p>
+          </a>
+          <div class="caption">
+            <p>Leviticus 15: 1 - 29</p>
+          </div>
         </div>
-        
-        <div class="col-xs-6 col-sm-3 col-md-3">
-            <a data-toggle="modal" data-target="#myModal">
-              <p class="bg-green" style="padding: 10px; min-height: 110px;">The use of Loin & Girdle</p>
-            </a>  
-            <div class="caption">  
-              <p>Matthew 17: 1 - 2 <br> Matthew 28: 2 - 3<br>Mark 9: 2 - 3<br>
-				Revelation 7: 9
-			  </p>
-            </div>  
+
+        <div class="col-xs-12 col-sm-6 col-md-3">
+            <a>
+              <p class="bg-green tenet">Black and Red clothes are forbidden except for educational or professional reasons</p>
+            </a>
+            <div class="caption">
+              <p>Revelations 6: 12<br>Revelations 19: 12 - 13<br></p>
+            </div>
         </div>
-        
-        <div class="col-xs-6 col-sm-3 col-md-3">
-            <a data-toggle="modal" data-target="#myModal">
-              <p class="bg-torqouise" style="padding: 10px">We use white cloth for the sutana we wear for worship.</p>
-            </a>  
-            <div class="caption">  
-              <p class="no-jumbotron-p">Matthew 17: 1 - 2 <br> Matthew 28: 2 - 3<br>Mark 9: 2 - 3<br>
-				Revelation 7: 9
-			  </p>
-            </div>  
+
+        <div class="col-xs-12 col-sm-6 col-md-3">
+            <a>
+              <p class="bg-blue tenet">Carrying corpse into the church is forbidden</p>
+            </a>
+            <div class="caption">
+              <p>Numbers 19: 12 - 14<br> Isaiah 38: 18<br>Ezekiel 43: 7 - 9<br>Mark 12: 27</p>
+            </div>
         </div>
-      
+
       </div>
       <hr>
-	  <div class="row">
-        
-        
-        <div class="col-xs-6 col-sm-3 col-md-3">
-            <a data-toggle="modal" data-target="#myModal">
-              <p class="bg-orange" style="padding: 10px">We must not wear shoes while wearing Sutana and whilst within the church premises</p>
-            </a>  
-            <div class="caption">  
-              <p>Matthew 17: 1 - 2 <br> Matthew 28: 2 - 3<br>Mark 9: 2 - 3<br>
-				Revelation 7: 9
-			  </p>
-            </div>  
-        </div>
-        
-		<div class="col-xs-6 col-sm-3 col-md-3">
-            <a data-toggle="modal" data-target="#myModal">
-              <p class="bg-blue" style="padding: 10px; min-height: 140px;">Chanting of Halleluyah to the four corners of the world</p>
-            </a>  
-            <div class="caption">  
-              <p>Matthew 17: 1 - 2 <br> Matthew 28: 2 - 3<br>Mark 9: 2 - 3<br>
-				Revelation 7: 9
-			  </p>
-            </div>  
-        </div>
-		
-        <div class="col-xs-7 col-sm-3 col-md-3">
-            <a>
-              <p class="bg-green" style="padding: 10px; min-height: 140px;">Women in menstruation are not allowed in the church until after sanctification</p>
-            </a>  
-            <div class="caption">  
-              <p>Matthew 17: 1 - 2 <br> Matthew 28: 2 - 3<br>Mark 9: 2 - 3<br>
-				Revelation 7: 9
-			  </p>
-            </div>  
-        </div>
-        
-        <div class="col-xs-6 col-sm-3 col-md-3">
-            <a>
-              <p style="padding: 10px; min-height: 140px;" class="bg-torqouise">Reciting "Holy Holy Holy is the Lord of Host" after the jingling of the bell</p>
-            </a>  
-            <div class="caption">  
-              <p class="no-jumbotron-p">Matthew 17: 1 - 2 <br> Matthew 28: 2 - 3<br>Mark 9: 2 - 3<br>
-				Revelation 7: 9
-			  </p>
-            </div>  
-        </div>
-      
-      </div>
-    
-	  <hr>
-	  <div class="row">
-        
-                <div class="col-xs-6 col-sm-3 col-md-3">
-            <a data-toggle="modal" data-target="#myModal">
-              <p class="bg-orange" style="padding: 10px; min-height: 140px;">Chanting of Halleluyah to the four corners of the world</p>
-            </a>  
-            <div class="caption">  
-              <p>Matthew 17: 1 - 2 <br> Matthew 28: 2 - 3<br>Mark 9: 2 - 3<br>
-				Revelation 7: 9
-			  </p>
-            </div>  
-        </div>
-        
-        <div class="col-xs-6 col-sm-3 col-md-3">
-            <a data-toggle="modal" data-target="#myModal">
-              <p class="bg-blue" style="padding: 10px; min-height: 140px;">We must not wear shoes while wearing Sutana and whilst within the church premises</p>
-            </a>  
-            <div class="caption">  
-              <p>Matthew 17: 1 - 2 <br> Matthew 28: 2 - 3<br>Mark 9: 2 - 3<br>
-				Revelation 7: 9
-			  </p>
-            </div>  
-        </div>
-        
-        <div class="col-xs-7 col-sm-3 col-md-3">
-            <a data-toggle="modal" data-target="#myModal">
-              <p class="bg-green" style="padding: 10px; min-height: 140px;">Women in menstruation are not allowed in the church until after sanctification</p>
-            </a>  
-            <div class="caption">  
-              <p>Matthew 17: 1 - 2 <br> Matthew 28: 2 - 3<br>Mark 9: 2 - 3<br>
-				Revelation 7: 9
-			  </p>
-            </div>  
-        </div>
-        
-        <div class="col-xs-6 col-sm-3 col-md-3">
-            <a data-toggle="modal" data-target="#myModal">
-              <p class="bg-torqouise" style="padding: 10px; min-height: 140px;">Reciting "Holy Holy Holy is the Lord of Host" after the jingling of the bell</p>
-            </a>  
-            <div class="caption">  
-              <p>Matthew 17: 1 - 2 <br> Matthew 28: 2 - 3<br>Mark 9: 2 - 3<br>
-				Revelation 7: 9
-			  </p>
-            </div>  
-        </div>
-      
-      </div>	
-	
-    </div>  
-  </div>
+	    <div class="row">
 
+        <div class="col-xs-12 col-sm-6 col-md-3">
+          <a>
+            <p class="bg-orange tenet">We must not wear shoes while wearing Sutana and whilst within the church premises</p>
+          </a>
+          <div class="caption">
+            <p>Matthew 17: 1 - 2<br>Matthew 28: 2 - 3<br>Mark 9: 2 - 3<br>Revelation 7: 9</p>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-3">
+            <a>
+              <p class="bg-torqouise tenet">Wearing shoes in the church premise or when in sutana is forbidden</p>
+            </a>
+            <div class="caption">
+              <p class="no-jumbotron-p">Joshua 5: 15 <br> Exodus 3: 2 - 5<br>Acts 7: 33<br> Revelations 1: 12 -15</p>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-3">
+          <a>
+            <p class="bg-green tenet">Alcohol and other intoxicants are strictly forbidden</p>
+          </a>
+          <div class="caption">
+            <p>Leviticus 10: 8 - 10<br> Genesis 9: 20 - 25<br>Proverbs 20: 1<br>Proverbs 23: 20 - 21</p>
+          </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-md-3">
+            <a>
+              <p class="bg-blue tenet">Washing of the feet is essential</p>
+            </a>
+            <div class="caption">
+              <p>Isaiah 28: 7 - 8 <br> Jeremiah 25: 15 - 27<br>Hosea 4: 11<br>Luke 1: 15</p>
+            </div>
+        </div>
+
+      </div>
+    <hr>
+	  <div class="row">
+      <div class="col-xs-12 col-sm-6 col-md-3">
+        <a>
+          <p class="bg-blue tenet">New moon service, is a divine injunction</p>
+        </a>
+        <div class="caption">
+          <p>Numbers 28: 11</p>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-3">
+        <a>
+          <p class="bg-orange tenet">Purification after child birth is forbidden</p>
+        </a>
+        <div class="caption">
+          <p>Leviticus 12: 1 - 8<br>Luke 2: 21 - 22<br>Mark 9: 2 - 3</p>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-3">
+        <a>
+          <p class="bg-green tenet">Protection and GOD's injunction according to the dictates of the holy spirit</p>
+        </a>
+        <div class="caption">
+          <p>Matthew 2: 13 - 19<br>Joshua 20: 1 - 6<br>Numbers 35: 6<br>Deuteronomy 4: 41 - 43</p>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-3">
+        <a>
+          <p class="bg-torqouise tenet">Harvest is GOD's order</p>
+        </a>
+        <div class="caption">
+          <p>Matthew 23: 14 - 17 <br> Matthew 13: 39<br>John 4: 34<br>Revelation 14: 15</p>
+        </div>
+      </div>
+
+    </div>
   </div>
+</div>
