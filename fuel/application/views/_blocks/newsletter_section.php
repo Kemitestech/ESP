43dfb1c81566031e77867792dc94964624462535
Emmanuel Unuigbe <unuigbee@hotmail.com>
2016-12-30 16:02:28 +0000

    fixed newsletter form layout issue

--- fuel/application/views/_blocks/newsletter_section.php
+++ fuel/application/views/_blocks/newsletter_section.php
@@ -1,11 +1,9 @@
   <div class="jumbotron newsletter-section">
     <div class="container">
-      <h3 class="header-title center-text">Sign up to our newsletter</h3><p class="center-text">Coming soon</p>
-      <div>
-        <form class="form-inline" style="width: 200px; margin: 0 auto;">
-          <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email">
-          <button type="submit" class="btn btn-default">Sign up</button>
-        </form>
-      </div>
+      <h2 class="header-title center-text">Sign up to our newsletter</h2>
+      <form class="form-inline form-center">
+        <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email">
+        <button type="submit" class="btn btn-default">Sign up</button>
+      </form>
     </div>
   </div>
