52596bd6c6dec5d9e834239601347b978ba8eebf
Emmanuel Unuigbe <unuigbee@hotmail.com>
2016-12-31 17:58:51 +0000

    updated newsletter UI

--- fuel/application/views/_blocks/newsletter_section.php
+++ fuel/application/views/_blocks/newsletter_section.php
@@ -1,9 +1,9 @@
   <div class="jumbotron newsletter-section">
     <div class="container">
-      <h2 class="header-title center-text">Sign up to our newsletter</h2>
+      <h2 class="header-title center-text">Subscribe to our newsletter</h2>
       <form class="form-inline form-center">
         <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email">
-        <button type="submit" class="btn btn-default">Sign up</button>
+        <button type="submit" class="btn btn-contact subscribe">Subscribe</button>
       </form>
     </div>
   </div>
