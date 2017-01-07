e58ad8a934d6376ffe3ab8912863f9726b3b0d42
Emmanuel Unuigbe <unuigbee@hotmail.com>
2017-01-07 03:42:06 +0000

    added page variable defaults

---
+++ fuel/application/views/_variables/media.php
@@ -0,0 +1,9 @@
+<?php
+$pages = array();
+$pages['media/:any'] = array('view' => 'media/index');
+
+$vars['view'] = 'media/index';
+$vars['page_title'] = 'Upcoming and Latest Events';
+$vars['open_graph_title'] = '';
+$vars['open_graph_description'] = '';
+$vars['open_graph_image'] = '';
