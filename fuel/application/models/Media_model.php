234766241fc75d95cb1b04d2873e647a624b6d69
Emmanuel Unuigbe <unuigbee@hotmail.com>
2017-01-12 21:10:03 +0000

    added comments to media model

--- fuel/application/models/Media_model.php
+++ fuel/application/models/Media_model.php
@@ -11,6 +11,7 @@ class Media_model extends CI_Model {
       //Codeigniter : Write Less Do More
   }
 
+  // Gets a list of media items based on project id and pagination criteria
   public function getMediaList($projectID = null, $page = 1, $per_page = 100, $full = true) {
       $medias = null;
 
@@ -35,35 +36,48 @@ class Media_model extends CI_Model {
       return $medias;
   }
 
+  // Gets a single nedia item based on media id
   public function mediaShow($id = null) {
       $media = $this->sendRequest('medias/'.$id);
 
       return $media;
   }
 
+
   protected function sendRequest($module, $params = null) {
       // build url
       $url = self::WISTIA_BASE_URL.$module.'.'.$this->format;
 
+      // checks if there params and appends them to url as query parameters
       if($params) {
           $url.='?'.http_build_query($params);
       }
+      // calls the final api endpoint
       $result = $this->__send($url);
+      // We then decode the JSON that's returned from the API
       $result = json_decode($result);
 
       return $result;
   }
 
   protected function __send($url) {
+      // initialise curl session with the url
       $ch = curl_init($url);
+
+      // we set our options for calling api
+      // Returns the transfer as a string
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-      curl_setopt($ch, CURLOPT_URL, $url);
+
+      // sets WWW-Authenticate for username:password
       curl_setopt($ch, CURLOPT_USERPWD, "api:".getenv("WISTIA_SECRET"));
 
+      // Execute the given cURL session.
       $result = curl_exec($ch);
+      // close the session
       curl_close($ch);
+      //sets the result to the response
       $this->response = $result;
-
+      // Then returns the result
       return $result;
   }
 }
