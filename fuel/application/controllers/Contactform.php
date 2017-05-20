9f9aa561b1c7be8d7df3f9e54321df836a988624
Emmanuel Unuigbe <unuigbee@hotmail.com>
2017-05-20 18:49:56 +0100

    updated Contact form

--- fuel/application/controllers/Contactform.php
+++ fuel/application/controllers/Contactform.php
@@ -8,10 +8,13 @@ class Contactform extends CI_Controller {
 
     public function index() {
 				$this->load->library('email');
-				$dotenv = new Dotenv\Dotenv(FCPATH);
-				$dotenv->load();
 				$spark_post_username = getenv('SPARK_POST_USERNAME');
 				$spark_post_password = getenv('SPARK_POST_SECRET');
+				$dotenv = new Dotenv\Dotenv(FCPATH);
+				$dotenv->load();
+				$client = new GuzzleHttp\Client();
+				$httpClient = new Http\Adapter\Guzzle6\Client($client);
+				$sparkPost = new SparkPost\SparkPost($httpClient, array('key' => getenv('SPARK_POST_SECRET').''));
 				$csrfTokenName = $this->security->get_csrf_token_name().'';
 				$csrfHash = $this->security->get_csrf_hash().'';
 
@@ -24,7 +27,6 @@ class Contactform extends CI_Controller {
 				$this->form_validation->set_rules('firstname', 'First Name', 'callback_check_empty');
 
         if($this->form_validation->run() == FALSE) {
-
             $this->output->set_status_header('400');
             $this->output->set_content_type('application/json');
 
@@ -36,49 +38,45 @@ class Contactform extends CI_Controller {
 						]);
         }
         else {
+           	$this->output->set_content_type('application/json');
+            $this->output->set_status_header('200');	
+
             $fullname = $this->input->post('fullname');
             $businessname = $this->input->post('businessname');
             $email = $this->input->post('email');
             $phone = $this->input->post('phone');
             $subject = $this->input->post('subject');
             $message = $this->input->post('message');
+						$textMessage = "Dear Edward Street Parish,\n\n{$message}\n\n{$businessname}\n\n{$phone}\n\nKind regards,\n{$fullname}";
 
-            $this->output->set_content_type('application/json');
-            $this->output->set_status_header('200');
-
-						$config['protocol'] = 'smtp';
-						$config['charset']  = 'utf-8'; //Change this you your preferred charset
-						$config['wordwrap'] = TRUE;
-						$config['mailtype'] = "html"; //Use 'text' if you don't need html tags and images
-						$config['crlf'] = "\r\n";      //should be "\r\n"
-						$config['newline'] = "\r\n";   //should be "\r\n"
-
-						$config['smtp_host'] = 'smtp.sparkpostmail.com';
-						$config['smtp_user'] = $spark_post_username;
-						$config['smtp_pass'] = $spark_post_password;
-						$config['smtp_port'] = '587';
-						$config['smtp_crypto'] = 'tls';
-
-						$this->email->initialize($config);
-            $this->email->from('info@cccedwardstreetparish.org', $fullname);
-						$this->email->reply_to($email, $fullname);
-            $this->email->to('info@cccedwardstreetparish.org');
-            $this->email->subject($subject);
-
-						//Checks if businessname and phonenumber is empty.
-						//If empty set variables to empty string
+						// Checks if businessname and phonenumber is empty.
+						// If empty set variables to empty string
 						$businessname = !empty($businessname) ? 'Business name: ' . $businessname . '<br><br>' : '';
 						$phone = !empty($phone) ? 'Contact Number: ' . $phone . '<br><br>' : '';
 						$conclusion = 'Kind regards,<br>' . $fullname;
 						$this->email->message('Dear Edward Street Parish,<br><br>' . $message . '<br><br>' . $businessname . $phone . $conclusion);
-
-            if(!$this->email->send()){
-                echo json_encode(array('result' => 'error', 'csrfTokenName' => $csrfTokenName, 'csrfHash' => $csrfHash));
-            }else{
-                echo json_encode(array('result' => 'ok', 'csrfTokenName' => $csrfTokenName, 'csrfHash' => $csrfHash));
-            }
+						$promise = $sparkPost->transmissions->post(array(
+							'content' => array(
+								'from' => array('name' => 'Edward Street Parish', 'email' => 'info@cccedwardstreetparish.org'),
+								'subject' => "{$subject}",
+								'html' => "Dear Edward Street Parish,<br><br>{$message}<br><br>{$businessname}{$phone}{$conclusion}",
+								'text' => $textMessage,
+								'reply_to' => "{$email}"
+							),
+							'recipients' => array( 
+									array( 
+										'address' => array('name' => 'Emmanuel', 'email' => 'info@cccedwardstreetparish.org')
+									) 
+							)
+						));	
+						
+						try {
+								$response = $promise->wait();
+								echo json_encode(array('result' => 'ok', 'csrfTokenName' => $csrfTokenName, 'csrfHash' => $csrfHash));
+						} catch (\Exception $e) {
+								echo json_encode(array('result' => 'error', 'csrfTokenName' => $csrfTokenName, 'csrfHash' => $csrfHash));
+						}											
         }
-
     }
 
 		public function check_empty($string) {
